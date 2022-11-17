69800d93226412f4804dcfe42a5693bab1fd2b24
NabJam <nabjam@MBP-de-NabJam.lan>
2022-11-17 22:31:37 +0100

    04 - rounded-pill form, picto homepage

---
+++ test2.php
@@ -0,0 +1,10 @@
+modification de la forme d'un champs de formulaire :
+    , {
+            'attr':{'class':'rounded-pill'},
+            label: "blabla"
+            }
+
+
+ancien code :
+{{ form_row(registrationForm.email) }}
+
